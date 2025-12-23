2c136416823ff074d9f5399bc97629b6bef37696
tpmx <info@tpmx.in>
2025-12-23 18:40:12 +0530

    inital

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -12,7 +12,7 @@ const SEO = ({
   const location = useLocation();
   const baseUrl = 'https://andhramachinesagencies.com';
   const currentUrl = `${baseUrl}${location.pathname}`;
-  const defaultImage = 'https://res.cloudinary.com/durbtkhbz/image/upload/v1766121553/5ce7960d-fb0f-4693-8c80-800e26fcac92-removebg-preview_cilmdc.png';
+  const defaultImage = 'https://res.cloudinary.com/do8cpljrz/image/upload/v1766379206/circle-cropped_wdduey_a0ileu.png';
   const defaultTitle = 'Andhra Machines Agencies - Premium Sewing Machines Since 1982';
   const defaultDescription = 'Your trusted partner for premium sewing machines since 1982. Shop Usha, Singer, Brother, Jack, Guru & Shiela sewing machines. Delivery across India. Expert service & genuine products.';
 
